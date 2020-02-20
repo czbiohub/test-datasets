3daba492bbcf3dd39ed0f007b8fd1619e0bcf8c5
Olga Botvinnik <olga.botvinnik@gmail.com>
2020-02-20 05:50:42 -0800

    Update reference data with PTPRC genes

--- reference/make_prot_accession2taxid_subset.sh
+++ reference/make_prot_accession2taxid_subset.sh
@@ -1,11 +1,20 @@
-# Use Bioawk to extract read ids of fasta
-bioawk -c fastx '{ print $name }' ncbi_refseq_vertebrate_mammalian_ptprc.fasta > ncbi_refseq_vertebrate_mammalian_ptprc_ids.txt
 
-# Create a header file for the tab-separated output
-zcat prot.accession2taxid.gz| head -n 1 > prot.accession2taxid.header.txt
+DIR=/mnt/ibm_lg/olga/czbiohub-reference/ncbi/refseq/releases/refseq-release98-2020-02-06/vertebrate_mammalian
+PTPRC_IDS=$DIR/ncbi_refseq_vertebrate_mammalian_ptprc_ids.txt
+PROT_ACCESSION=prot.accession2taxid.gz
+HEADER=prot.accession2taxid.header.txt
 
-# Find the read ids in the gzipped file, add the heaer
-zgrep --fixed-strings -f \
-	~/data_lg/czbiohub-reference/ncbi/refseq/releases/refseq-release98-2020-02-06/vertebrate_mammalian/ncbi_refseq_vertebrate_mammalian_ptprc_ids.txt prot.accession2taxid.gz  \
-	| cat prot.accession2taxid.header.txt -  \
-	| gzip -c - > prot.accession2taxid.vertebrate_mammalian_ptprc.with_header.txt.gz
+echo "Extracting PTPRC sequence ids ..."
+time bioawk -c fastx '{ print $name }' $DIR/ncbi_refseq_vertebrate_mammalian_ptprc.fasta > $PTPRC_IDS
+echo "\tDone!"
+
+echo "Making header for protein accession tsv ..."
+time zcat $PROT_ACCESSION | head -n 1 > $HEADER
+echo "\tDone!"
+
+echo "Searching for the PTPRC sequence ids in the prot2accession file ..."
+time zgrep --fixed-strings \
+      -f $PTPRC_IDS $PROT_ACCESSION \
+    | cat $HEADER -  \
+    | gzip -c - > prot.accession2taxid.vertebrate_mammalian_ptprc.with_header.txt.gz
+echo "\tDone!"
--- reference/make_ptprc_subset.py
+++ reference/make_ptprc_subset.py
@@ -2,12 +2,17 @@
 import screed
 import glob
 import os
-option1 = 'protein tyrosine phosphatase, receptor type, C'
-option2 = 'receptor-type tyrosine-protein phosphatase C'
+option1 = 'protein tyrosine phosphatase, receptor type, c'
+option2 = 'receptor-type tyrosine-protein phosphatase c'
+option3 = 'PTPRC'
+option4 = 'leukocyte common antigen'
+options = option1, option2, option3, option4
+
 with open('ncbi_refseq_vertebrate_mammalian_ptprc.fasta', 'w') as f:
 
     for filename in glob.iglob('*faa.gz'):
         for record in screed.open(filename):
-            if option2 in record['name'] or option1 in record['name']:
+            name = record['name'].lower()
+            if any(option in name for option in options):
                 f.write(f">{record['name']}\n{record['sequence']}\n")
                 
